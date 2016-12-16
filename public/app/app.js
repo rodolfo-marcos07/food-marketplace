a093a1e0f2cd6dbbaa0886600e8f4c9c7dfc1561
Rodolfo Luis Marcos <rodolfo.marcos@webb.main>
2016-12-16 14:53:00 -0200

    Funcionou App08!!

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,44 @@
 var appModule = angular.module('app08', []);
 
+// Firebase Integration
+var config = {
+	apiKey: "AIzaSyCWZjtflhSSUI59zBs-gqqgUloor2ZGXtI",
+	authDomain: "app-08.firebaseapp.com",
+	databaseURL: "https://app-08.firebaseio.com",
+	storageBucket: "app-08.appspot.com",
+	messagingSenderId: "893322157672"
+};
+firebase.initializeApp(config);
+
 // Main Controller
-appModule.controller('mainCtrl', function($scope){
+appModule.controller('mainCtrl', function($scope, itemService){
 	var main = $scope;
 	main.teste = "App 08 beggins";
+	$scope.item = {};
+	$scope.itens = itemService.obter();
+	$scope.salvar = function(){
+		itemService.salvar($scope.item.titulo, $scope.item.desc);
+	}
+});
+
+appModule.factory('itemService', function($http){
+	// Get a reference to the database service
+	var database = firebase.database();
+	var service = {
+		salvar: function(t, d){
+			// Get the key when inserting
+			var key = database.ref().child('itens').push().key
+			var updates = {};
+			var toSave = {titulo:t, descricao:d};
+			updates['/itens/'+key] = toSave;
+			return database.ref().update(updates);
+		},
+		obter: function(){
+			var itensRef = database.ref('itens/');
+			itensRef.on('value', function(snapshot){
+				console.log(snapshot.val());
+			});
+		}
+	};
+	return service;
 });
